1e020946e098d031373f50460d0870900b5408ae
Prajwal <prajwalgnprajwal7@gmail.com>
2026-05-30 21:29:05 +0530

    initial project setup with canvas

---
+++ src/graphics.h
@@ -0,0 +1,12 @@
+#ifndef GRAPHICS_H
+#define GRAPHICS_H
+
+#define WIDTH 80
+#define HEIGHT 25
+
+extern char canvas[HEIGHT][WIDTH];
+
+void clearCanvas();
+void displayCanvas();
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,8 @@
+#include "graphics.h"
+
+int main()
+{
+    clearCanvas();
+    displayCanvas();
+    return 0;
+}
